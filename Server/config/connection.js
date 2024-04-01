33a5b8f49588501a227b2284b69769e17c880185
Corey Donahue <cdartswebdev@gmail.com>
2024-04-01 13:45:52 -0600

    added p.JSON too root, added app name "readradar" and changed connection.js for mongoose to "readradar"

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 // Connect to the ReadRadar database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ReadRadar', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/readradar', { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = mongoose.connection;
