928d839d5d4209649bb9249c3a7093421a043419
Corey Donahue <cdartswebdev@gmail.com>
2024-04-02 10:12:16 -0600

    updated server.js to connect to mongodb://localhost:27017/readradar

--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,7 @@ const axios = require('axios'); // Import Axios for making HTTP requests
 require('dotenv').config();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/readradar', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => console.log('Connected to MongoDB'));
