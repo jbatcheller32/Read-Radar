cb63a5ced7f124cdb35cca223862911fc812bc53
mazoal1982 <aloys.manga@gmail.com>
2024-04-01 18:56:20 -0600

    Initial Commit

---
+++ mern-read-radar/src/components/edit-read.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditReads extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on the Edit Reads component!</p>
+      </div>
+    );
+  }
+}
---
+++ mern-read-radar/src/components/reads-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ReadsList extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on the Reads List component!</p>
+      </div>
+    );
+  }
+}
