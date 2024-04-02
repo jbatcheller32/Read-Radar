a1d5923b41ebeaef505ba6da591eb33a5135318d
Corey Donahue <cdartswebdev@gmail.com>
2024-04-02 11:14:05 -0600

    Server running on port 3002 Use GraphQL at http://localhost:3002/graphql Connected to MongoDB

--- Server/server.js
+++ Server/server.js
@@ -29,6 +29,6 @@ const server = new ApolloServer({ typeDefs, resolvers });
   await server.start();
   server.applyMiddleware({ app });
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  const PORT = process.env.PORT || 3002; // Change this line
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}\nUse GraphQL at http://localhost:${PORT}${server.graphqlPath}`));
 })();
