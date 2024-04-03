7b98588a26c5011525124ab09ce13fecfcf3da98
Aloys Z Manga <91248776+mazoal1982@users.noreply.github.com>
2024-04-03 00:02:14 -0600

    Delete Server directory

--- Server/config/connection.js
+++
@@ -1,8 +0,0 @@
-const { connect, connection } = require('mongoose');
-
-// Connect to the ReadRadar database
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/readradar'
-
-connect(connectionString);
-
-module.exports = connection;
