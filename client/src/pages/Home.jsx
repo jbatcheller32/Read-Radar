001e49919dabe64a3e41263dfc43205887538bd6
mazoal1982 <aloys.manga@gmail.com>
2024-04-09 01:19:18 -0600

    tried getting images to load but cannot add second api call to resule call in jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,14 +4,17 @@ import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import '../styles/styles.css';
 import './Home.css'
+import { searchGoogleBooks } from '../utils/API'
 
 function BookSearch() {
 
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [results, setResults] = useState([]);
+  const [items, setItems] = useState([]);
   const [savedBookIds, setSavedBookIds] = useState([]);
   const [saveBook] = useMutation(SAVE_BOOK);
+
 
 
 
@@ -31,20 +34,64 @@ function BookSearch() {
     }
   };
 
-  const handleSearchSubmit = (e) => {
+  const handleSearchSubmit = async (e) => {
     e.preventDefault();
 
-    setTitle('');
-    setAuthor('');
+    try {
+      const response = await searchGoogleBooks("harry+potter");
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+      const { items } = await response.json();
+      const bookData = items.map((book) => ({
+        image: book.volumeInfo.imageLinks?.thumbnail || '',
+      }));
+      console.log(bookData)
+      setItems(bookData);
+    } catch (err) {
+      console.error(err);
+    }
 
   };
 
   // this handle click function fetches the book data from the API
   const handleClick = async () => {
     try {
+      const Imgresponse = await searchGoogleBooks("harry+potter");
+
+      if (!Imgresponse.ok) {
+        throw new Error('something went wrong!');
+      }
+      const { items } = await Imgresponse.json();
+      const bookData = items.map((book) => ({
+        image: book.volumeInfo.imageLinks?.thumbnail || '',
+      }));
+      console.log(bookData)
+      setItems(bookData);
       const response = await fetch(`https://openlibrary.org/search.json?title=${title}`);
       const data = await response.json();
-
+      let img0 = bookData[0].image;
+      let img1 = bookData[1].image;
+      let img2 = bookData[2].image;
+      let img3 = bookData[3].image;
+      let img4 = bookData[4].image;
+      let img5 = bookData[5].image;
+      let img6 = bookData[6].image;
+      let img7 = bookData[7].image;
+      let img8 = bookData[8].image;
+      let img9 = bookData[9].image;
+      console.log(data)
+      data.docs.img0 = bookData[0].image
+      data.docs.img1 = bookData[1].image
+      data.docs.img2 = bookData[2].image
+      data.docs.img3 = bookData[3].image
+      data.docs.img4 = bookData[4].image
+      data.docs.img5 = bookData[5].image
+      data.docs.img6 = bookData[6].image
+      data.docs.img7 = bookData[7].image
+      data.docs.img8 = bookData[8].image
+      data.docs.img9 = bookData[9].image
       setResults(data.docs);
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -100,7 +147,7 @@ function BookSearch() {
       <h1>
         Search
       </h1>
-      <form className="form" onSubmit={handleSearchSubmit}>
+      <form className="form" value={items} onSubmit={handleSearchSubmit}>
         <input
           value={title}
           name="title"
@@ -122,12 +169,14 @@ function BookSearch() {
 
       <section className="results">
         {results.map((result, index) => (
+
           <div className="book-results-container" key={index}>
             <header className="w3-container w3-blue">
               <u><h2>{result.title}</h2></u>
+              <img src={result.img0} alt="" />
             </header>
 
-            <div clasNames="w3-container">
+            <div className="w3-container">
               <i><p>Author: {result.author_name ? result.author_name.join(', ') : 'Unknown'}</p></i>
               <p>Genre: {result.subject ? result.subject.join(', ') : 'Unknown'}</p>
             </div>
