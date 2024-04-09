0b8fb43357f8cbd94b64daf89a225f734d4285ba
Corey Donahue <cdartswebdev@gmail.com>
2024-04-08 21:20:39 -0600

    change utils/auth to utils/Auth

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
 
-import Auth from '../../utils/auth'; 
+import Auth from '../../utils/Auth'; 
 
 const Header = () => {
     const logout = (e) => {
